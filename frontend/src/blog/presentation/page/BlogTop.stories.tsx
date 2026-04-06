8b1f3750644c222b392b4923764bdd7dbc302f0e
nawawan <mizutake0428@yahoo.co.jp>
2026-04-06 23:20:00 +0900

    chore: move files to other directry

--- frontend/src/blog/presentation/page/BlogTop.stories.tsx
+++ frontend/src/blog/presentation/page/BlogTop.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import BlogTop  from './BlogTop';
-import * as EntryCardStories from "./EntryCards/EntryCard.stories";
-import * as SidebarStories from "./Sidebar/Sidebar.stories";
-import EntryCard from "./EntryCards/EntryCard";
-import Sidebar from './Sidebar/Sidebar';
+import * as EntryCardStories from "../EntryCards/EntryCard.stories";
+import * as SidebarStories from "../Sidebar/Sidebar.stories";
+import EntryCard from "../EntryCards/EntryCard";
+import Sidebar from '../Sidebar/Sidebar';
 
 const meta = {
     title : "BlogTop",
