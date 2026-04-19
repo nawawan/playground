650bc10e9a7d5e665bbdb95d4942afef4eebaa6b
nawawan <mizutake0428@yahoo.co.jp>
2026-04-19 15:38:51 +0900

    chore: update import path

--- frontend/src/blog/container/page/Container.tsx
+++ frontend/src/blog/container/page/Container.tsx
@@ -1,7 +1,7 @@
 
 import { BlogTop } from "../../presentation/page/blogs/BlogTop";
 
-import { useGenerateProps } from "./blogs/useGenerateProps";
+import { useGenerateProps } from "./useGenerateProps";
 
 export const BlogTopContainer = () => {
     const generatedProps = useGenerateProps();
