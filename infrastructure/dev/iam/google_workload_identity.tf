9b93b4dc0330908822149f73486c5a6e712df42f
nawawan <mizutake0428@yahoo.co.jp>
2026-04-26 19:37:12 +0900

    chore: separate wif

---
+++ infrastructure/dev/iam/google_workload_identity.tf
@@ -0,0 +1,65 @@
+resource "google_iam_workload_identity_pool" "nawawan_dev_pool" {
+  workload_identity_pool_id = "nawawan-dev-pool"
+  display_name              = "nawawan dev pool"
+  description               = "Workload Identity Pool for Nawawan dev actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "nawawan_dev_github_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.nawawan_dev_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-dev-reader"
+  display_name                       = "GitHub actions Provider"
+  description                        = "Workload Identity Pool Provider for GitHub Actions"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
+    "attribute.ref_type"   = "assertion.ref_type"
+    "attribute.event_name" = "assertion.event_name"
+  }
+
+  attribute_condition = <<EOT
+    assertion.repository == "nawawan/playground" &&
+    assertion.event_name in ["pull_request", "workflow_dispatch"]
+  EOT
+}
+
+resource "google_iam_workload_identity_pool_provider" "nawawan_dev_github_provider_editor" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.nawawan_dev_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-dev-editor"
+  display_name                       = "GitHub actions editor Provider"
+  description                        = "Workload Identity Pool Provider for GitHub Actions"
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.ref"        = "assertion.ref"
+    "attribute.ref_type"   = "assertion.ref_type"
+  }
+
+  attribute_condition = <<EOT
+    assertion.repository == "nawawan/playground" &&
+    assertion.ref_type == "branch" &&
+    assertion.ref == "refs/heads/main"
+  EOT
+}
+
+resource "google_service_account_iam_member" "nawawan_dev_reader_member" {
+  service_account_id = data.google_service_account.nawawan_dev_reader.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.nawawan_dev_pool.name}/attribute.repository/nawawan/playground"
+}
+
+resource "google_service_account_iam_member" "nawawan_dev_editor_member" {
+  service_account_id = data.google_service_account.nawawan_dev_editor.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.nawawan_dev_pool.name}/attribute.repository/nawawan/playground"
+}
--- infrastructure/prod/google_service_accounts.tf
+++ infrastructure/prod/google_service_accounts.tf
@@ -4,22 +4,8 @@ resource "google_service_account" "nawawan_prod_reader" {
   display_name = "Nawawan Prod Reader"
 }
 
-resource "google_service_account_iam_member" "nawawan_prod_reader_member" {
-  provider           = google
-  service_account_id = google_service_account.nawawan_prod_reader.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.nawawan_prod_pool.name}/attribute.repository/nawawan/playground"
-}
-
 resource "google_service_account" "nawawan_prod_editor" {
   provider     = google
   account_id   = "nawawan-prod-editor"
   display_name = "Nawawan Prod Editor"
-}
-
-resource "google_service_account_iam_member" "nawawan_prod_editor_member" {
-  provider           = google
-  service_account_id = google_service_account.nawawan_prod_editor.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.nawawan_prod_pool.name}/attribute.repository/nawawan/playground"
 }
