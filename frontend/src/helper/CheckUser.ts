1eebabaadfdaa980797613c697cb0e1e816b0a10
nawawan <mizutake0428@yahoo.co.jp>
2026-04-29 17:29:37 +0900

    chore: verify only jwt existence

--- frontend/src/helper/CheckUser.ts
+++ frontend/src/helper/CheckUser.ts
@@ -1,5 +1,6 @@
-// 認証済みは管理者しかいない
-export const checkAuthorizedUser = () => {
+// 認証済み(ログインユーザー)は管理者しかいない。
+// そのため、JWTがないユーザだけ弾く
+export  const checkAuthorizedUser = () => {
     try {
         const cfToken = document.cookie
             .split(';')
@@ -7,16 +8,7 @@ export const checkAuthorizedUser = () => {
             .find(c => c.startsWith('CF_Authorization='))
             ?.split('=')[1];
 
-        if (!cfToken) return false;
-
-        // JWT payload は base64url エンコードなので standard base64 に変換
-        const base64 = cfToken.split('.')[1]
-            .replace(/-/g, '+')
-            .replace(/_/g, '/')
-            .padEnd(Math.ceil(cfToken.split('.')[1].length / 4) * 4, '=');
-
-        const payload = JSON.parse(atob(base64));
-        return !!payload.email;
+       return !!cfToken;
     } catch {
         return false;
     }
