949ffc7714fda9a1efc66c36eb0622e4a763518c
nawawan <mizutake0428@yahoo.co.jp>
2026-05-17 20:22:43 +0900

    chore: update validation

--- frontend/src/presentation/widgets/MarkdownEditor/validation/SubmitBlogValidation.ts
+++ frontend/src/presentation/widgets/MarkdownEditor/validation/SubmitBlogValidation.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const MarkdownEditorSchema = z.object({
-    title: z.string().min(0),
+    title: z.string().optional(),
     slug: z
         .string()
-        .min(0)
-        .regex(/^[a-z0-9-]*$/, '英小文字・数字・ハイフンのみ使用できます'),
+        .regex(/^[a-z0-9-]*$/, '英小文字・数字・ハイフンのみ使用できます')
+        .optional(),
 });
