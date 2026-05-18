2141a6bace6e7f8c967e87b9177055e13ac9e8cc
nawawan <mizutake0428@yahoo.co.jp>
2026-05-19 01:53:24 +0900

    feat: create prod cloud run infra

--- infrastructure/prod/google_secret_manager.tf
+++ infrastructure/prod/google_secret_manager.tf
@@ -5,3 +5,39 @@ resource "google_secret_manager_secret" "github_token" {
     auto {}
   }
 }
+
+resource "google_secret_manager_secret" "prod_cloudflare_access_key_id" {
+  secret_id = "prod_cloudflare_access_key_id"
+
+  labels = {
+    label = "prod_cloudflare_access_key_id"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "prod_cloudflare_secret_access_key" {
+  secret_id = "prod_cloudflare_secret_access_key"
+
+  labels = {
+    label = "prod_cloudflare_secret_access_key"
+  }
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret" "prod_cloudflare_account_id" {
+  secret_id = "prod_cloudflare_account_id"
+
+  labels = {
+    label = "prod_cloudflare_account_id"
+  }
+
+  replication {
+    auto {}
+  }
+}
