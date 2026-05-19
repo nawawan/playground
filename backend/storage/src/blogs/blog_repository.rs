5c6df44b124d6d5abda734a769f7995031bb63d7
nawawan <mizutake0428@yahoo.co.jp>
2026-05-20 05:44:34 +0900

    chore: apply formatter

--- backend/storage/src/blogs/blog_repository.rs
+++ backend/storage/src/blogs/blog_repository.rs
@@ -32,8 +32,9 @@ impl BlogRepository for Repository {
     }
 
     async fn list_blogs(&self, filter: BlogFilter) -> Vec<Blog> {
-        let mut builder =
-            sqlx::QueryBuilder::new("SELECT id, title, slug, content_key, status FROM blogs WHERE 1=1");
+        let mut builder = sqlx::QueryBuilder::new(
+            "SELECT id, title, slug, content_key, status FROM blogs WHERE 1=1",
+        );
         filter.apply(&mut builder);
         builder
             .build_query_as::<Blog>()
